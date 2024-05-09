cbf7c451737402f048fd9d01d410517549e5cd2d
David Lucas <david.lucas95@gmail.com>
2024-05-08 18:55:46 -0600

    set up basic file structure and dependencies basic file wireframe with dependencies.

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//creating variables
+const app = express();
+const PORT = process.env.SERVER_PORT;
+const MONGO = process.env.MONGODB;
+
+//connecting to database
+mongoose.connect(`${MONGO}`);
+
+//creating database variable
+const db = mongoose.connection;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+//printing to the console when connection is established
+db.once('open', () => console.log(`connected to Mongo Atlas DB`));
+
+//printing to the console what port is being used
+app.listen(PORT, () => { console.log(`listening on port: ${PORT}`) })
