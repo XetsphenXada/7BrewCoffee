c691798b7fc78afd6c7dd0a3d042f7fc3d227b49
Jordan Marble <jamarble8@gmail.com>
2024-05-15 16:40:49 -0600

    Retrieves flashcard data from json and send data from get request.

--- server/controllers/flashcards.js
+++ server/controllers/flashcards.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import flashcardData from "../initial-data/flashcardData.json" with { type: "json" };
 
 const router = Router();
 
+// return all flashcards
 router.get("/flashcards", (request, response) => {
-    response.send("hi")
+    try {
+        // get all flashcard data
+        const flashcards = flashcardData;
+
+        // send flashcard data as response
+        response.send(flashcards);
+    }
+    catch(err) {
+        response.status(500).send({
+            message: err.message
+        });
+    }
 });
 
 export default router;
