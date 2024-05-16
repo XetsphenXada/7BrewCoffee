a42f338e61ad78ea1148e4d6e0bde4173ed3c4b7
Jordan Marble <jamarble8@gmail.com>
2024-05-16 17:26:38 -0600

    Creates flippable flashcards

--- client/src/components/Flashcards.jsx
+++ client/src/components/Flashcards.jsx
@@ -2,21 +2,45 @@ export default function Flashcards() {
 
     return (
         <div className="stack">
-            <div className="card w-96 h-48 shadow-md bg-primary text-primary-content">
-                <div className="card-body flex-col justify-center">
-                    <h2 className="card-title justify-center">flashcard1</h2>
+            <label className="swap swap-flip">
+                <input type="checkbox" />
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 front</h2>
+                    </div>
+                </div> 
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 back</h2>
+                    </div>
                 </div>
-            </div> 
-            <div className="card w-96 h-48 shadow-md bg-primary text-primary-content">
-                <div className="card-body flex-col justify-center">
-                    <h2 className="card-title justify-center">flashcard2</h2>
+            </label>
+            <label className="swap swap-flip">
+                <input type="checkbox" />
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 front</h2>
+                    </div>
+                </div> 
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 back</h2>
+                    </div>
                 </div>
-            </div> 
-            <div className="card w-96 h-48 shadow-md bg-primary text-primary-content">
-                <div className="card-body flex-col justify-center">
-                    <h2 className="card-title justify-center">flashcard3</h2>
+            </label>
+            <label className="swap swap-flip">
+                <input type="checkbox" />
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 front</h2>
+                    </div>
+                </div> 
+                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
+                    <div className="card-body flex-col justify-center">
+                        <h2 className="card-title justify-center">flashcard1 back</h2>
+                    </div>
                 </div>
-            </div> 
+            </label>
         </div>
     )
 }
