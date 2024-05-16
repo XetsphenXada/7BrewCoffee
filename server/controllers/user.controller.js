7f6d4ba4c8cfde256fff38d0532fd0458fb622dd
Marck Bashaw <marckbashaw@gmail.com>
2024-05-15 20:31:58 -0400

    Added a manager/admin Signup endpoint, will require a adminCheck middleware to check if the user signing up is currently an admin

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,21 +1,49 @@
 import User from "../models/users.js";
+import Permission from "../models/permissions.js"
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const router = Router();
 
-//Admin signup endpoint
+//Manager/Admin signup endpoint
 router.post("/signup", async (request, response) => {
     try {
         //checks to see if the user exists
         const doesUserExist = await User.exists({
             email: request.body.email
         });
-        
+        if (doesUserExist === null) {
+            const user = new User({
+                employeeID: request.body.employeeID,
+                firstName: request.body.firstName,
+                middleName: request.body.middleName,
+                lastName: request.body.lastName,
+                role: request.body.role,
+                storeLocation: request.body.storeLocation,
+                brewistas: request.body.brewistas,
+                email: request.body.email,
+                passwordHash: request.body.passwordHash
+            });
+            
+            await user.save();
+            
+            const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+            
+            response.send({
+                message: "Success",
+                token: token
+            });
+        } else {
+            response.status(500).send({
+                message: "Email is already in use"
+            });
+        }
     } catch (error) {
-        
-    }
+        response.status(500).send({
+            message: error.message
+        });
+    };
 });
 
 //User login endpoint
@@ -42,5 +70,7 @@ router.post("/login", async (request, response) => {
         })
     }
 });
+
+
 
 export default router;
