64268a9811f0626457be5123bee1ed73c959c510
Jordan Marble <jamarble8@gmail.com>
2024-05-16 21:50:52 -0600

    Lays out flashcard data onto stacked cards with array mapping.

--- client/src/components/Flashcards.jsx
+++ client/src/components/Flashcards.jsx
@@ -1,59 +1,35 @@
 import { useEffect, useState } from "react";
 
 export default function Flashcards() {
-    let flashcards;
+    const [flashcards, setFlashcards] = useState();
 
     useEffect(() => {
         async function getFlashcardData() {
             const response = await fetch("http://localhost:3000/flashcards");
-            flashcards = await response.json();
+            const flashcardJson = await response.json();
+            setFlashcards(flashcardJson)
         }
         
         getFlashcardData();
     }, []);
-    
 
     return (
         <div className="stack">
-            <label className="swap swap-flip">
-                <input type="checkbox" />
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
+            {flashcards.map((card) => (
+                <label key={card.cardNumber} className="swap swap-flip">
+                    <input type="checkbox" />
+                    <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
                     <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 front</h2>
+                        <h2 className="card-title justify-center">{card.question}</h2>
                     </div>
-                </div> 
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
-                    <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 back</h2>
+                    </div> 
+                    <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
+                        <div className="card-body flex-col justify-center">
+                            <h2 className="card-title justify-center">{card.answer}</h2>
+                        </div>
                     </div>
-                </div>
-            </label>
-            <label className="swap swap-flip">
-                <input type="checkbox" />
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
-                    <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 front</h2>
-                    </div>
-                </div> 
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
-                    <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 back</h2>
-                    </div>
-                </div>
-            </label>
-            <label className="swap swap-flip">
-                <input type="checkbox" />
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-off">
-                    <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 front</h2>
-                    </div>
-                </div> 
-                <div className="card w-96 h-48 shadow-md bg-primary text-primary-content flex-col justify-center swap-on">
-                    <div className="card-body flex-col justify-center">
-                        <h2 className="card-title justify-center">flashcard1 back</h2>
-                    </div>
-                </div>
-            </label>
+                </label>
+            ))}
         </div>
     )
 }
