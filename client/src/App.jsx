dcbc2dfe86b4f248e285b8e7bb84aeb1a086061e
David Lucas <david.lucas95@gmail.com>
2024-05-28 16:02:45 -0600

    desktop page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom'
 // import Flashcards from './components/Flashcards'
 import Auth from './components/auth/Auth'
-import { useState } from 'react';
 import AdminSignUp from './components/auth/AdminSignUp';
+import Layout from './components/testpagelayoutcode';
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
           element={<Auth setToken={setToken} />} //pass update component as props
         />
 		<Route path="/signup" element={<AdminSignUp />}/>
+    <Route path="/layout" element={<Layout />}/>
       	</Routes>
 		</>
 	)
