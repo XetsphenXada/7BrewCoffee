3f64dd9a74fce7c5dad73a7d4b5275f5d06ad940
Jordan Marble <jamarble8@gmail.com>
2024-05-29 18:46:31 -0600

    Starts route to get list of all quiz names

--- server/controllers/quizzes.js
+++ server/controllers/quizzes.js
@@ -8,9 +8,27 @@ router.get("/quiz/:quizParam", (request, response) => { // !! will need user val
     try {
         // find quiz questions that match quizParam
         const quizQuestions = allQuizzes.filter((quiz) => quiz.quizParam === request.params.quizParam);
-        
+
         // send quiz data as response
         response.send(quizQuestions);
+    }
+    catch(err) {
+        response.status(500).send({
+            message: err.message
+        });
+    }
+});
+
+// return list of quiz names
+router.get("/quiznames", (request, response) => {
+    try {
+        // make array of different quiz names
+        const quizNames = allQuizzes.filter((quiz) => {
+            
+        });
+        
+        // send quiz names as array
+        response.send(quizNames)
     }
     catch(err) {
         response.status(500).send({
