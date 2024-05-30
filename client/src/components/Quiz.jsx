ccce3ed1b2da1ab78fc0179b8218c067849764bd
Jordan Marble <jamarble8@gmail.com>
2024-05-30 16:55:56 -0600

    Adds buttons for future form submission

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -17,19 +17,19 @@ export default function Quiz() {
     }, []);
 
     return (
-        <div className="flex flex-col m-5">
-            <div>{quiz[0].quizName}</div>
+        <div className="flex flex-col items-center m-5">
+            <div className="text-5xl mb-7">{quiz[0].quizName}</div>
             <form>
                 {quiz.map((singleQuestion, i) => (
-                    <div>
-                        <div key={i}>{singleQuestion.question}</div>
+                    <>
+                        <div key={i} className="text-2xl">{singleQuestion.question}</div>
                         {singleQuestion.questionType === "selectAll" ? (
                             <>
                                 {Object.values(singleQuestion.answerChoices).map((choice, j) => (
                                     <div className="form-control flex-row">
                                         <label className="label cursor-pointer">
                                             <input type="checkbox" name={`checkbox-${i}`} className="checkbox m-3" />
-                                            <span key={j}>{choice}</span>
+                                            <span key={j} className="text-lg">{choice}</span>
                                         </label>
                                     </div>
                                 ))}
@@ -46,9 +46,13 @@ export default function Quiz() {
                                 ))}
                             </>
                         )}
-                    </div>
+                    </>
                 ))}
             </form>
+            <div className="flex justify-around w-full">
+                <button className="btn btn-neutral">Cancel</button>
+                <button className="btn btn-accent">Submit</button>
+            </div>
         </div>
     )
 }
