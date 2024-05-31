bc2e7634ba5f88138adfb0e3566526aae9bebea2
David Lucas <david.lucas95@gmail.com>
2024-05-31 17:35:35 -0600

    removed some routes and imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 // import Flashcards from './components/Flashcards'
 import Auth from './components/auth/Auth'
 import AdminSignUp from './components/auth/AdminSignUp';
-import Layout from './components/testpagelayoutcode';
 import EmpCreation from './components/auth/EmpCreation';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Email from './components/auth/Password-Reset-Email-Page';
 
 
@@ -21,10 +18,7 @@ function App() {
           element={<Auth setToken={setToken} />} //pass update component as props
         />
 		<Route path="/signup" element={<AdminSignUp />}/>
-    <Route path="/layout" element={<Layout />}/>
 		<Route path="/adduser" element={<EmpCreation />}/>
-		<Route path="/header" element={<Header />}/>
-		<Route path="/footer" element={<Footer />}/>
 		<Route path="/email" element={<Email />}/>
       	</Routes>
 		</>
