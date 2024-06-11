583865cf4bd779ff8489e7b2c1d80a6e473a1fcb
Marck Bashaw <marckbashaw@gmail.com>
2024-06-10 20:05:02 -0400

    Changes to routing before merge to main (attempting to pre-resolve some conflicts)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom'
-// import Flashcards from './components/Flashcards'
-import Auth from './components/auth/Auth'
-import AdminSignUp from './components/auth/AdminSignUp';
-import Layout from './components/testpagelayoutcode';
-import EmpCreation from './components/auth/EmpCreation';
-import ViewAllUsers from './components/auth/ViewAllUsers';
-import EditProfileCard from './components/auth/EditProfileCard';
+
 
 
 function App() {
@@ -14,17 +8,7 @@ function App() {
 	
 	return (
 		<>
-		<Routes>
-        <Route
-          path="/user/login"
-          element={<Auth setToken={setToken} />} //pass update component as props
-        />
-		<Route path="/addAdmin" element={<AdminSignUp />}/>
-    	<Route path="/layout" element={<Layout />}/>
-		<Route path="/addEmployee" element={<EmpCreation />}/>
-		<Route path="/allusers" element={<ViewAllUsers />}/>
-		<Route path="/edit/:_id" element={<EditProfileCard />}/>
-      	</Routes>
+
 		</>
 	)
 }
