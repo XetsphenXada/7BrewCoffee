21015379df7b6f4598d671b7d672a3fcf828f840
Jordan Marble <jamarble8@gmail.com>
2024-06-18 16:09:49 -0600

    Centers error page element

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -2,8 +2,8 @@ import logo from "../images/sevenBrew_Secondary.png"
 
 export default function ErrorPage() {
     return (
-        <>
+        <div className="flex justify-center">
             <img src={logo} className="opacity-25 size-4/5" />
-        </>
+        </div>
     )
 }
