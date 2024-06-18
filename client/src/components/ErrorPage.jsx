239bcedf33440023e150f5c4ca59b81ca2028306
Jordan Marble <jamarble8@gmail.com>
2024-06-17 20:51:19 -0600

    Adds semi-transparent background image to error page

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,3 +1,9 @@
+import logo from "../images/sevenBrew_Secondary.png"
+
 export default function ErrorPage() {
-    return <div></div>
+    return (
+        <>
+            <img src={logo} className="opacity-25 size-4/5" />
+        </>
+    )
 }
